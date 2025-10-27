ec0617107544d6623d79591a878dd1faac02ba42
farahelnakhal <farah.q.el@gmail.com>
2025-10-27 21:51:25 +0400

    Add solution for Euler problem 1
    Implement solution for Euler problem 1 to sum multiples of 3 or 5 below a given number.

--- problem_001.py
+++ problem_001.py
@@ -1,1 +1,11 @@
+# Problem 1: Multiples of 3 or 5
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below a given number.
 
+#function to solve euler problem
+def euler_1(num): 
+  #for every i that is divisible by 3 or 5, add it to the sum
+  print(sum(i for i in range(num) if i % 3 == 0 or i % 5 == 0))
+
+#call the function on the requested value
+euler_1(1000)
